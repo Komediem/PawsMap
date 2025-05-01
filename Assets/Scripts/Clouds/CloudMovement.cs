daa6d0d9f81406fd463a1c8c1a87b1c2853f22fe
Gabbbbbbbbbb <gcochard@artfx.fr>
2025-05-01 18:53:51 +0200

    [dev] Clouds disappear animation

--- Assets/Scripts/Clouds/CloudMovement.cs
+++ Assets/Scripts/Clouds/CloudMovement.cs
@@ -13,25 +13,40 @@ public class CloudMovement : MonoBehaviour
     [SerializeField] private int MinCloudOpacity;
     [SerializeField] private int MaxCloudOpacity = 255;
     [SerializeField] private bool IsTopBottomCloud = false;
+    [SerializeField] private int HidingThreshold;
 
     [Header("Private Data")]
     public float Speed;
     [SerializeField] public Vector2 Objective;
     [SerializeField] private int CloudLayer;
     [SerializeField] public int ID;
+    private int Opacity;
 
     void Start()
     {
         Speed = Random.Range(MinSpeed, MaxSpeed);
         Speed = Speed * Time.deltaTime;
 
-        float CloudRandomOpacity = Random.Range(MinCloudOpacity, MaxCloudOpacity);
+        int CloudRandomOpacity = Random.Range(MinCloudOpacity, MaxCloudOpacity);
         CloudRandomOpacity = CloudRandomOpacity / 255;
         GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, CloudRandomOpacity);
+        Opacity = CloudRandomOpacity;
     }
 
     void Update()
     {
+        if (GameManager.Instance.currentSize > HidingThreshold)
+        {
+            float i = Mathf.Lerp(Opacity, 0, Time.deltaTime);
+            this.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, i);
+        }
+        else if (GameManager.Instance.currentSize <= HidingThreshold)
+        {
+            //this.GetComponent<SpriteRenderer>().enabled = true;
+
+        }
+
+
         if (IsTopBottomCloud == false)
         {
             transform.position = Vector2.MoveTowards(transform.position, Objective, Speed);
