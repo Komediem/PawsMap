ef5a80010c31e0612048b39084be5c07f7d51e11
Gabbbbbbbbbb <gcochard@artfx.fr>
2025-05-07 00:08:47 +0200

    [dev] Cloud smoothly appear & disappear

--- Assets/Scripts/Clouds/CloudMovement.cs
+++ Assets/Scripts/Clouds/CloudMovement.cs
@@ -25,18 +25,34 @@ public class CloudMovement : MonoBehaviour
     [HideInInspector] public Vector2 Objective;
     [HideInInspector] public int ID;
     private float Opacity;
+    private bool IsAppearing = true;
+    private bool IsDestroying = false;
 
     void Start()
     {
+        //Sets the Speed of the Cloud.
         Speed = Random.Range(MinSpeed, MaxSpeed);
         Speed = Speed * Time.deltaTime;
 
+        //Sets a random opacity between two values.
         float CloudRandomOpacity = Random.Range(MinCloudOpacity, MaxCloudOpacity);
         CloudRandomOpacity = CloudRandomOpacity / 255;
-        GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, CloudRandomOpacity);
+        
+        if(IsTopBottomCloud == true)
+        {
+            GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, CloudRandomOpacity);
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 0);
+        }
+
         Opacity = CloudRandomOpacity;
 
+        //Sets the threshold at which the cloud fades aways when zooming in.
         HidingThreshold = Random.Range(MinHidingThreshold, MaxHidingThreshold);
+
+
     }
 
     void Update()
@@ -53,21 +69,54 @@ public class CloudMovement : MonoBehaviour
         }
 
 
+        if(IsAppearing & IsTopBottomCloud == false)
+        {
+            SpawningOpacityChange();
+        }
+
         if (IsTopBottomCloud == false)
         {
             transform.position = Vector2.MoveTowards(transform.position, Objective, Speed);
 
             if (transform.position == new Vector3(Objective.x, Objective.y, 0))
             {
-                Destroy(gameObject);
                 CloudSystem.Instance.NumberOfClouds -= 1;
+                IsDestroying = true;
             }
         }
         if (IsTopBottomCloud == true)
         {
             transform.Translate(Vector2.right * Speed);
         }
+
+        if(IsDestroying == true)
+        {
+            float i = Mathf.Lerp(this.GetComponent<SpriteRenderer>().color.a, 0, Time.deltaTime * 5);
+            this.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, i);
+
+            if(GetComponent<SpriteRenderer>().color.a <= i)
+            {
+                Debug.Log("HAAAHHHHHHH");
+                Destroy(gameObject);
+            }
+        }
     }
+
+    void SpawningOpacityChange()
+    {
+        if(GetComponent<SpriteRenderer>().color.a != Opacity)
+        {
+            float i = Mathf.Lerp(this.GetComponent<SpriteRenderer>().color.a, Opacity, Time.deltaTime / 10);
+            this.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, i);
+            Debug.Log("AHHHH");
+        }
+        else
+        {
+            IsAppearing = false;
+            Debug.Log("A");
+        }
+    }
+
 
     public GameObject TpObject;
     void OnTriggerEnter2D(Collider2D col)
