870a2a6e02e1b0e09d4965cbdb3945994e761f24
Gabbbbbbbbbb <gcochard@artfx.fr>
2025-05-08 19:43:48 +0200

    [add] Import & Cloud Center

--- Assets/Scripts/Clouds/CloudStorm.cs
+++ Assets/Scripts/Clouds/CloudStorm.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Experimental.GraphView.Port;
 
 public class CloudStorm : MonoBehaviour
 {
 
     [SerializeField] private GameObject CloudCenter;
     [SerializeField] private float Speed;
+    [SerializeField] private float HidingThreshold;
 
     // Start is called before the first frame update
     void Start()
@@ -17,7 +19,17 @@ public class CloudStorm : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //CloudCenter.transform.rotation = Quaternion.Slerp(CloudCenter.transform.rotation, CloudCenter.transform.rotation, Speed);
         CloudCenter.transform.Rotate(Vector3.forward, Speed);
+
+        if (GameManager.Instance.currentSize < HidingThreshold)
+        {
+            float i = Mathf.Lerp(CloudCenter.GetComponent<SpriteRenderer>().color.a, 0, Time.deltaTime * 3);
+            CloudCenter.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, i);
+        }
+        else if (GameManager.Instance.currentSize > HidingThreshold)
+        {
+            float i = Mathf.Lerp(CloudCenter.GetComponent<SpriteRenderer>().color.a, 1, Time.deltaTime * 3);
+            CloudCenter.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, i);
+        }
     }
 }
